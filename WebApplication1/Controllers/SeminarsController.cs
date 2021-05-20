ee8808081ebbfcbbb13ef73cfd5888195ff26d60
Anant Jaiswal <jaiswalanant339@gmail.com>
2021-05-20 23:56:08 +0530

    initial commit

---
+++ WebApplication1/Controllers/SeminarsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class SeminarsController : Controller
+    {
+        private SeminarManagementEntities1 db = new SeminarManagementEntities1();
+
+        // GET: Seminars
+        public ActionResult Index()
+        {
+            return View(db.Seminars.ToList());
+        }
+
+        // GET: Seminars/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seminar seminar = db.Seminars.Find(id);
+            if (seminar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seminar);
+        }
+
+        // GET: Seminars/Create
+        public ActionResult Create()
+        {
+            ViewBag.Organizer = db.Organizers.ToList();
+            return View();
+        }
+
+        // POST: Seminars/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SeminarID,OrganizerID,SeminarTitle,SeminarDescription,SeminarType,SeminarDate,SeminarStartTime,SeminarEndTime,CreatedDate")] Seminar seminar)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Seminars.Add(seminar);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(seminar);
+        }
+
+        // GET: Seminars/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seminar seminar = db.Seminars.Find(id);
+            if (seminar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seminar);
+        }
+
+        // POST: Seminars/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SeminarID,OrganizerID,SeminarTitle,SeminarDescription,SeminarType,SeminarDate,SeminarStartTime,SeminarEndTime,CreatedDate")] Seminar seminar)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(seminar).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(seminar);
+        }
+
+        // GET: Seminars/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seminar seminar = db.Seminars.Find(id);
+            if (seminar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seminar);
+        }
+
+        // POST: Seminars/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Seminar seminar = db.Seminars.Find(id);
+            db.Seminars.Remove(seminar);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
